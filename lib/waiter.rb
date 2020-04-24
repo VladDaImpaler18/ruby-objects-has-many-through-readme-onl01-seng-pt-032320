e06fc39fe7e99d04f6885ce2247138bc0209083f
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-24 17:42:26 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -6,11 +6,23 @@ def initialize(name, years_of_experience)
   @name = name
   @years = years_of_experience
   @@all << self
-  
 end
 
 def self.all
   @@all
+end
+
+def new_meal
+  Meal.new(self, customer_obj, total, tip=0)
+end
+
+def meals
+  Meal.all.select{|m| m.waiter == self}
+end
+
+def best_tipper
+  best=0
+  meals.select{|m| best = m.tip if best < m.tip}
 end
 
 end
